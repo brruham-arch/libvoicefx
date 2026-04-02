a1ce29feb4ebbb437d8b36fb01fb5c8d042a1b12
brruham-arch <brruham-arch@users.noreply.github.com>
2026-04-02 14:34:56 +0700

    fix: entry point OnModLoad + link libstdc++

--- jni/Android.mk
+++ jni/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := voicefx
 LOCAL_SRC_FILES := voicefx.c
 LOCAL_CFLAGS    := -O2 -ffast-math -fPIC -Wall
-LOCAL_LDLIBS    := -lm -llog -ldl
+LOCAL_LDLIBS    := -lm -llog -ldl -lstdc++
 LOCAL_ARM_MODE  := arm
 
 include $(BUILD_SHARED_LIBRARY)
