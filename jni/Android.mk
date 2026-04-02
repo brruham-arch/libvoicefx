fd98c2c966f084488a2ac166a978f05b2193dc32
brruham-arch <brruham-arch@users.noreply.github.com>
2026-04-02 16:50:08 +0700

    fix ld flag

--- jni/Android.mk
+++ jni/Android.mk
@@ -6,6 +6,6 @@ LOCAL_MODULE    := voicefx
 LOCAL_SRC_FILES := voicefx.cpp
 LOCAL_CPPFLAGS  := -std=c++17 -fPIC
 LOCAL_CFLAGS    := -Wall
-LOCAL_LDLIBS    := -lm -llog -ldl -static-libstdc++ -Wl,--export-all-symbols
+LOCAL_LDLIBS    := -lm -llog -ldl -Wl,--export-all-symbols
 
 include $(BUILD_SHARED_LIBRARY)
